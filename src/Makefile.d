5e60c5f1fcef1a6dfaf18c9d8765dde94ed8a86d
chaosink <475936052@qq.com>
2015-12-16 21:17:31 +0800

    modify Makefile, remove useless files

--- src/Makefile.d
+++ src/Makefile.d
@@ -1,19 +1,37 @@
 LIB = -Djava.ext.dirs=lib
+
 ROOT = JTalk/
 SERVER = $(ROOT)server/
 SERVER_MODEL = $(SERVER)model/
+SERVER_VIEW= $(SERVER)view/
+SERVER_CONTROLLER = $(SERVER)controller/
 SERVER_UTIL = $(SERVER)util/
-TARGET = $(SERVER_MODEL)JTDBAccount.class
-SRC = $(TARGET:%.class=%.java)
-CLASS = $(TARGET:%.class=%)
+DIR = $(SERVER_MODEL) $(SERVER_MODEL) $(SERVER_CONTROLLER) $(SERVER_UTIL)
+
+TARGET = $(TARGET1) $(TARGET2) $(TARGET3)
+TARGET1 = $(SERVER_MODEL)JTDBAccount.class
+TARGET2 = $(SERVER_MODEL)JTDBFriend.class
+TARGET3 = $(SERVER_MODEL)JTDBOfflineMessage.class
 
 all: $(TARGET)
 
-$(TARGET): $(SRC)
-	@ javac $(SRC)
+$(TARGET1): $(@:%.class=%.java)
+	@ javac $(@:%.class=%.java)
 
-run:
-	@ java $(LIB) $(CLASS)
+$(TARGET2): $(@:%.class=%.java)
+	@ javac $(@:%.class=%.java)
+
+$(TARGET3): $(@:%.class=%.java)
+	@ javac $(@:%.class=%.java)
+
+run1:
+	@ java $(LIB) $($(@:run%=TARGET%):%.class=%)
+
+run2:
+	@ java $(LIB) $($(@:run%=TARGET%):%.class=%)
+
+run3:
+	@ java $(LIB) $($(@:run%=TARGET%):%.class=%)
 
 clean:
-	@ rm -f $(TARGET)
+	@ rm -f $(DIR:%=%*.class)
