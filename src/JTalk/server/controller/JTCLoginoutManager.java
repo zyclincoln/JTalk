51982851cdbd21a763d90533885278aaaf6caaee
zyclincoln <zyclincoln@gmail.com>
2015-12-26 08:52:04 +0800

    loginout login function

--- src/JTalk/server/controller/JTCLoginoutManager.java
+++ src/JTalk/server/controller/JTCLoginoutManager.java
@@ -16,29 +16,69 @@ public class JTCLoginoutManager{
 	public LoginLog Login(int id,String password,String loginIP){
 		AccountCheckResult result=database.CheckIn(id,password,calendar.getTimeInMillis(),loginIP);
 		LoginLog log;
-		if(result.result==0){
-			loginTable.Login(id,loginIP);
-			GetFriendResult database.GetFriend(id);
+		String errorCause;
+		Boolean isSuc=true;
 
+		if(result.result==0){
+			//loginTable.Login(id,loginIP);
+			HashMap<Integer,String> friends = new HashMap<Integer,String>();
+			ArrayList<OfflineMessage> offlineMessage;
+			GetFriendResult gfResult database.GetFriend(id);
+			if(gfResult.result_number==0){
+				for(int index = 0;index < gfResult.friend.size();index++){
+					AccountSearchResult asResult database.SearchAccountInfo(gfResult.friend.get(index));
+					if(asResult.result==0){
+						friends.put(gfResult.friend.get(index),asResult.name);
+					}
+					else{
+						errorCause="Cannot get friend : "+gfResult.friend.get(index);
+						isSuc=false;
+						break;
+					}
+				}
+				if(isSuc==true){
+					GetMessageResult gmResult database.GetMessage(id);
+					if(gmResult.result_number==0){
+						offlineMessage=gmResult.offline_message;
+						SPLogin reply=new SPLogin(result.name,)
+						loginTable.Login(id,loginIP,friends,offlineMessage);
+						if(loginTable.getSender(id).Deliver(reply)){
+							log = new LoginLog(true,result,"Login succeed : "+id);
+							return log;
+						}
+						else{
+							errorCause="deliver reply error.";
+						}
+					}
+					else{
+						errorCause="get OfflineMassage error";
+					}
+				}
+				else{
+					errorCause="get Friend info error";
+				}
+			}
+			else{
+				errorCause="get Friend id error";
+			}
 		}
-		else{
-			ObjectOutputStream toClient;
+
+		ObjectOutputStream toClient;
+		try{
+			Socket client=new Socket(loginIP,8000);
+			toClient=new ObjectOutputStream(client.getOutputStream());
+			toClient.writeObject(new SPLogin());
+			log=new LoginLog(false,result,"Login failed :"+errorCause);
+		}
+		catch(Exception e){
+			log=new LoginLog(false,result,"Login failed : "+e.toString());
+		}
+		finally{
 			try{
-				Socket client=new Socket(loginIP,8000);
-				toClient=new ObjectOutputStream(client.getOutputStream());
-				toClient.writeObject(new SPLogin());
-				log=new LoginLog(false,result,"Login failed :"+"id");
+				toClient.close();
 			}
 			catch(Exception e){
-				log=new LoginLog(false,result,"Login failed : "+e.toString());
-			}
-			finally{
-				try{
-					toClient.close();
-				}
-				catch(Exception e){
-					System.out.println(e);
-				}
+				System.out.println(e);
 			}
 		}
 		return log;
--- src/JTalk/server/util/SPLogin.java
+++ src/JTalk/server/util/SPLogin.java
@@ -4,12 +4,12 @@ import java.util.*;
 public class SPLogin extends ServerPackage{
 	public boolean isSuc;
 	public String name;
-	public ArrayList<String> friends;
+	public HashMap<Integer,String> friends;
 	public ArrayList<OfflineMessage> offlineMessage;
 	public SPLogin(){
 		this.isSuc=false;
 	}
-	public SPLogin(String name,ArrayList<String> friends,ArrayList<OfflineMessage> offlineMessage){
+	public SPLogin(String name,HashMap<Integer,String> friends,ArrayList<OfflineMessage> offlineMessage){
 		this.isSuc=true;
 		this.name=name;
 		this.friends=friends;
