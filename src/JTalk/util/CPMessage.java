bc2b22780a4ce9ffec007f436e9c2f30b2bef372
chaosink <475936052@qq.com>
2015-12-26 09:04:43 +0800

    SPMessage.java CPMessage.java

--- src/JTalk/util/CPMessage.java
+++ src/JTalk/util/CPMessage.java
@@ -1,14 +1,19 @@
 package JTalk.util;
 
-public class CPMessage extends ClientPackage{
-	public int reciver_id;
+public class CPMessage extends ClientPackage {
+	public int type;
+	public int sender_id;
+	public int receiver_id;
 	public int message_id;
 	public long time;
-	public String contents;
-	public CPMessage(int revicer_id, int message_id, long time, String contents){
-		this.reciver_id=reciver_id;
-		this.message_id=message_id;
-		this.time=time;
-		this.contents=contents;
+	public String content;
+
+	public CPMessage(int type, int sender_id, int revicer_id, int message_id, long time, String content) {
+		this.type = type;
+		this.sender_id = sender_id;
+		this.reciver_id = reciver_id;
+		this.message_id = message_id;
+		this.time = time;
+		this.content = content;
 	};
 }
--- src/JTalk/util/SPMessage.java
+++ src/JTalk/util/SPMessage.java
@@ -1,5 +1,17 @@
 package JTalk.util;
 
-public class SPMessage extends ServerPackage{
+public class SPMessage extends ServerPackage {
+	public int type;
+	public int sender_id;
+	public int message_id;
+	public long time;
+	public String content;
 
+	SPMessage(int type, int sender_id, int message_id, long time, String content) {
+		this.type = 0;
+		this.sender_id = sender_id;
+		this.message_id = message_id;
+		this.time = time;
+		this.content = content;
+	}
 }
