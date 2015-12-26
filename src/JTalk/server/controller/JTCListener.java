073565a79adf5a50a05cac8db437a9b940005c01
zyclincoln <zyclincoln@gmail.com>
2015-12-26 12:03:55 +0800

    JTCListener finish

---
+++ src/JTalk/server/controller/JTCListener.java
@@ -0,0 +1,41 @@
+package JTalk.server.controller;
+import JTalk.server.util.*;
+import JTalk.util.*;
+import java.net.*;
+import java.io.*;
+import java.util.concurrent.*;
+
+public class JTCListener implements Runnable{
+	ServerSocket serverSocket;
+	ExecutorService executor;
+	JTCSignupManager signupManager;
+	JTCLoginoutManager loginoutManager;
+	JTCMessageManager messageManager;
+	JTCMessageReceivedManager messageReceiveManager;
+	public JTCListener(JTCSignupManager signupManager, JTCLoginoutManager loginoutManager,
+	 JTCMessageManager messageManager, JTCMessageReceivedManager messageReceiveManager){
+		try{
+			serverSocket=new ServerSocket(8086);
+		}
+		catch(IOException e){
+			System.out.println("Fatal: JTCListener initial failed."+e);
+		}
+		executor=Executors.newCachedThreadPool();
+		this.signupManager=signupManager;
+		this.loginoutManager=loginoutManager;
+		this.messageReceiveManager=messageReceiveManager;
+		this.messageManager=this.messageManager;
+	}
+
+	public void run(){
+		try{
+			while(true){
+				Socket client=serverSocket.accept();
+				executor.execute(new JTCJobManager(client,signupManager,loginoutManager,messageManager,messageReceiveManager));
+			}
+		}
+		catch(IOException e){
+			System.out.println("Warning: JTCListener.run error." +e);
+		}
+	}
+}
