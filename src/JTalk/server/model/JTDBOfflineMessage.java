0995e672769576a8da0114f8743bc6aae1276678
zyclincoln <zyclincoln@gmail.com>
2015-12-26 08:27:16 +0800

    test

--- src/JTalk/server/model/JTDBOfflineMessage.java
+++ src/JTalk/server/model/JTDBOfflineMessage.java
@@ -7,25 +7,6 @@ import java.util.Calendar;
 import java.util.ArrayList;
 import java.sql.*;
 
-class OfflineMessage {
-	int type;
-	int sender_id;
-	int message_id;
-	long time;
-	String content;
-
-	OfflineMessage(int type, int sender_id, int message_id, long time, String content) {
-		this.type = 0;
-		this.sender_id = sender_id;
-		this.message_id = message_id;
-		this.time = time;
-		this.content = content;
-	}
-
-	public String toString() {
-		return String.format("type: %d    sender_id: %d    message_id: %d    time: %d    content: %s", type, sender_id, message_id, time, content);
-	}
-}
 
 class GetMessageResult implements Message {
 	int result_number;
