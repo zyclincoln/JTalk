d5bde086f103dcc0e3ec745984f4054ad460806e
chaosink <475936052@qq.com>
2015-12-27 09:40:03 +0800

    client db testing done

--- src/JTalk/client/model/JTDatabase.java
+++ src/JTalk/client/model/JTDatabase.java
@@ -22,6 +22,7 @@ public class JTDatabase {
 	void AddAccount(int id, HashMap<Integer, String> friends) {
 		try {
 			statement.executeUpdate("CREATE TABLE Friend" + id + " (id int, name char(20), primary key(id))");
+			System.out.println("Account " + id + " added");
 		} catch(Exception e) {
 			System.out.println(e);
 		}
@@ -42,7 +43,8 @@ public class JTDatabase {
 	void AddFriend(int id, int friend_id, String friend_name) {
 		try {
 			statement.executeUpdate("insert into Friend" + id + " values(" + friend_id + ", '" + friend_name + "')");
-			statement.executeUpdate("create table Message" + id + "_" + friend_id + " (type int, time bigint, content varchar(255))");		
+			statement.executeUpdate("create table Message" + id + "_" + friend_id + " (type int, time bigint, content varchar(255))");
+			System.out.println(id + "'s friend " + friend_id + " added");
 		} catch(Exception e) {
 			System.out.println(e);
 		}
@@ -51,13 +53,28 @@ public class JTDatabase {
 	void AddMessage(int id, OfflineMessage offline_message) {
 		try {
 			statement.executeUpdate("insert into Message" + id + "_" + offline_message.sender_id + " values(" + offline_message.type + ", " + offline_message.time + ", '" + offline_message.content + "')");
+			System.out.println(id + "'s message added");
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	void DeleteFriend(int id, int friend_id) {
+		try {
+			statement.executeUpdate("drop table Message" + id + "_" + friend_id);
+			statement.executeUpdate("delete from Friend" + id + " where id = " + friend_id);
+			System.out.println(id + "'s friend " + friend_id + " removed");
 		} catch(Exception e) {
 			System.out.println(e);
 		}
 	}
 
 	void Terminate() {
-
+		try {
+			connection.close();
+		} catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 
 	public static void main(String[] args) {
@@ -69,6 +86,8 @@ public class JTDatabase {
 		db.AddAccount(1, map);
 		db.AddFriend(1, 4, "4");
 		db.AddMessage(1, new OfflineMessage(0, 2, 2, 2, "2"));
+		db.DeleteFriend(1, 4);
+		db.DeleteFriend(1, 2);
 		db.Terminate();
 	}
 }
